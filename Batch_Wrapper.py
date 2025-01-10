e1d20de567eb9dbc1e501f5f62551b72292d6d72
TryHarder <brian8204001@gmail.com>
2025-01-10 12:52:52 -0500

    Added Batch_Wrapper.py

---
+++ Batch_Wrapper.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+class Batch(tf.keras.layers.Layer):
+    def __init__(self, operation, **kwargs):
+        super().__init__(**kwargs)
+        self.operation = operation
+
+    @tf.function
+    def call(self, inputs):
+        return tf.vectorized_map(self.operation, inputs)
