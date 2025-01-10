452179f8c3b5fc37a67176a17762c593d2aaafcd
TryHarder <brian8204001@gmail.com>
2025-01-10 15:09:33 -0500

    Changed the output head

--- model.py
+++ model.py
@@ -5,21 +5,22 @@ from Grok_Model import Grok_Fast_EMA_Model
 import matplotlib.pyplot as plt
 
 # 1
-def make_model(embed_size, num_head=1, num_layers=2):
-    inputs = tf.keras.layers.Input(batch_shape=(None, None, 50), name="inputs")
+def make_toy_model(embed_size, num_head=1, num_layers=2):
+    inputs = tf.keras.layers.Input(batch_shape=(None, None, 100), name="inputs")
 
     x = Batch(tf.keras.layers.Dense(embed_size))(inputs)
 
     for layer_id in range(num_layers):
         x = RWKV_Block(layer_id, num_head, embed_size)(x)
 
-    x = Batch(tf.keras.layers.Dense(6))(x)
+    x = Batch(tf.keras.layers.Dense(6 * 100))(x)
+    x = tf.keras.layers.Reshape((-1, 100, 6))(x)
 
     outputs = tf.keras.layers.Activation("softmax")(x)
     return Grok_Fast_EMA_Model(inputs=inputs, outputs=outputs)
 
 if __name__ == "__main__":
-    model = make_model(128, 1, 2)
+    model = make_toy_model(128, 1, 2)
     model.summary()
 
 
